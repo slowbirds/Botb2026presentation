cd397a226ac463c36e36b09f073a2161a1eb75a2
vfa-akato <slowbirds@gmail.com>
2026-03-25 22:36:07 +0700

    fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   base: '/',
@@ -14,7 +15,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 
